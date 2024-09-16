9a3f20e31f09e3282fe79b8e75f4bf49e5b30994
zaynetcom <obingwae@gmail.com>
2024-09-16 16:23:31 +0700

    Add files via upload

---
+++ main.js
@@ -0,0 +1,17 @@
+const harga1 = document.getElementById("price1");
+const harga2 = document.getElementById("price2");
+const harga3 = document.getElementById("price3");
+const harga4 = document.getElementById("price4");
+const harga5 = document.getElementById("price5");
+
+
+//harga 1
+harga1.textContent = "165.000/bulan";
+//harga 2
+harga2.textContent = "228.000/bulan";
+//harga 3
+harga3.textContent = "260.000/bulan";
+//harga 4
+harga4.textContent = "338.000/bulan";
+//harga 5
+harga5.textContent = "670.000/bulan";
